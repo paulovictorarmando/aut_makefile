f044b1fa0e1b64e38e8662ecb1c17aebf961b167
Paulo Armando <122297062+paulovictorarmando@users.noreply.github.com>
2025-10-16 12:23:59 +0100

    Refactor srcs and headers handling in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pathlib
 import sys
 
+
 if len(sys.argv) != 5:
     print("Erro, forma de usar: ")
     print("python3 main.py linguagem(c/cpp) compilador(cc/c++) nome_exec \"flags(-Wall -Wextra -Werror)\"")
@@ -18,30 +19,36 @@ except:
     pass
 
 
-srcs = [
-            [ str(f) for f in pathlib.Path('./').rglob('*')
-                if f.is_file()
-                and (f.suffix == '.' + 'h' if sys.argv[1].lower() == 'c' else 'hpp') 
-            ],
-            [ str(f) for f in pathlib.Path('./').rglob('*')
-                if f.is_file()
-                and (f.suffix == '.' + 'c' if sys.argv[1].lower() == 'c' else 'cpp') 
-            ]
-        ]
+srcs =  [
+		str(f) for f in pathlib.Path('./').rglob('*')
+		if f.is_file() and (
+			f.suffix == '.cpp' and sys.argv[1].lower() == 'cpp'
+			or
+			f.suffix == '.c' and sys.argv[1].lower() == 'c'
+		)
+	]
+headers =[
+		str(f) for f in pathlib.Path('./').rglob('*')
+		if f.is_file() and (
+			f.suffix == '.hpp' and sys.argv[1].lower() == 'cpp'
+			or
+			f.suffix == '.h' and sys.argv[1].lower() == 'c'
+		) 
+	]
 
 with open("./Makefile", "a+") as file:
     file.write(f"NAME = {sys.argv[3]}\n\n")
     file.write(f"HEAD = ")
-    for f in srcs[0]:
+    for f in headers:
         file.write(f"\t\t\t{f}\\\n")
     file.write("\n")
     file.write(f"SRC = ")
-    for f in srcs[1]:
+    for f in srcs:
         file.write(f"\t\t\t{f}\\\n")
     file.write("\n")
     file.write(f"CXX = {sys.argv[2].lower()}\n\n")
     file.write(f"CXXFLAGS = {sys.argv[4]}\n\n")
-    file.write(f"OBJ = $(SRC:.{srcs[1][0].split('.')[1]}=.o)\n\n")
+    file.write(f"OBJ = $(SRC:.{sys.argv[1].lower()}=.o)\n\n")
     file.write(f"all: $(NAME)\n\n")
     file.write(f"$(NAME): $(OBJ) $(HEAD)\n")
     file.write(f"\t$(CXX) $(CXXFLAGS) $(OBJ) -o $(NAME)\n\n")
